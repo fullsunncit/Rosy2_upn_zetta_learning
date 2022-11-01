0a7f0dcdf81783207711bf0b4497df1e08db1c7a
fullsunncit <nurrosyd13@gmail.com>
2022-11-01 11:40:02 +0700

    c

---
+++ my-app/src/app/cashier/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  @Input('items') items: Item[]=[]
+  @Output('itemAdded') onAddItem: EventEmitter<Item> = new EventEmitter<Item>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addItem(item: Item): void {
+    this.onAddItem.emit(item)
+  }
+
+}
