935178fbdeb6389520ce1ebf11f27828b86aa523
fullsunncit <nurrosyd13@gmail.com>
2022-11-02 21:06:18 +0700

    angular day 4

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CashierComponent } from './cashier/cashier/cashier.component';
+import { MenuComponent } from './cashier/menu/menu.component';
+import { PaymentComponent } from './cashier/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CashierComponent,
+    MenuComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
